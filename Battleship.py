f77ba20e729c0bc2e9d970cea4bff58f31355968
Tal <tal@server.fake>
2014-05-27 09:41:36 -0600

    Added AI for computer:
    If computer hits something, it doesn't shoot randomly anymore - it shoots to the left or right of the place it hit

--- Battleship.py
+++ Battleship.py
@@ -7,6 +7,7 @@ __author__ = 'tal'
 Grid_Size = 10
 PC_location = [random.randint(0, Grid_Size - 1), random.randint(0, Grid_Size - 1)]
 CHEATING = True  # Shows you what the computer grid looks like
+DIFFICULTY = "Hard"
 
 
 def player_message():
@@ -34,11 +35,34 @@ def signal_handler(signal, frame):
 
 def computer_turn():
     while True:
-        rnd = [random.randint(0, Grid_Size - 1), random.randint(0, Grid_Size - 1)]
+        if DIFFICULTY == "Easy":
+            shot = [random.randint(0, Grid_Size - 1), random.randint(0, Grid_Size - 1)]
+        elif DIFFICULTY == "Hard":
+            #The list that stores all the locations of hits
+            hits = []
+            #AI. Shoots randomly until it hit something
+            #When it does, it either shoots to the left or right
+            #of that spot
+            for y in range(0, Grid_Size):
+                for x in range(0, Grid_Size):
+                    if Player_Grid[x][y].ship_here and Player_Grid[x][y].already_tried:
+                        hits.append([x, y])
+            #If we've hit something before
+            if len(hits) > 0:
+                #If we shot to the right of the right-most previous hit, would we hit a wall?
+                test_shot = [hits[len(hits) - 1][0] + 1, hits[len(hits) - 1][1]]
+                if (test_shot[0] <= Grid_Size - 1) and Player_Grid[test_shot[0]][test_shot[1]].already_tried is False:
+                    shot = test_shot
+                else:
+                    #Go left instead
+                    shot = [hits[0][0] - 1, hits[0][1]]
+            else:
+                #We've hit nothing. Guess again
+                shot = [random.randint(0, Grid_Size - 1), random.randint(0, Grid_Size - 1)]
 
         #Make sure the computer hasn't shot that spot before
-        if not Player_Grid[rnd[0]][rnd[1]].already_tried:
-            Player_Grid[rnd[0]][rnd[1]].already_tried = True
+        if not Player_Grid[shot[0]][shot[1]].already_tried:
+            Player_Grid[shot[0]][shot[1]].already_tried = True
 
             #Clear screen
             os.system('clear 2>/dev/null')
@@ -48,8 +72,8 @@ def computer_turn():
 
             #Write message to screen what the shot was, and if it was a hit or miss
             print '\n'
-            print 'Computer has shot (%d,%d).' % (rnd[0], rnd[1]),
-            if Player_Grid[rnd[0]][rnd[1]].ship_here:
+            print 'Computer has shot (%d,%d).' % (shot[0], shot[1]),
+            if Player_Grid[shot[0]][shot[1]].ship_here:
                 print "HIT!"
             else:
                 print "MISS!"
